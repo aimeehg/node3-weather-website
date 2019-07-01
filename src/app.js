a871aa694abe8abc0db0c1990b9cebdd5044689d
Aimee Hernandez <hernandez.aimee@outlook.com>
2019-07-01 13:26:32 -0500

    add about text

--- src/app.js
+++ src/app.js
@@ -37,7 +37,7 @@ app.get('/about', (req,res) =>{
 app.get('/help', (req, res) =>{
     res.render('help',{
         title: 'Help',
-        message: 'this is a message',
+        message: 'Help page',
         name: 'Aimee Hernandez'
     })
 })
